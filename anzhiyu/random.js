01120a9e0d2f192816cde96f4ce3dc41f3de7223
chickingoneb <chickingoneb@gmail.com>
2025-04-15 00:13:47 +0800

    Site updated: 2025-04-15 00:13:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["chick/这是一篇新的博文/"];function toRandomPost(){
+var posts=["chickhexo/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
